bd59a7d313d20cc57557b15c3fc7ea7202bc4953
ReDoomVII <audrickaudrick4@gmail.com>
2024-09-21 17:14:02 +0700

    Ini Tugas Studi Kasus 8 Audrick XI TIP 1

---
+++ 21-09-2024/StudyKasus8.sh
@@ -0,0 +1,38 @@
+function hitungharga() {
+    echo "Berapa jumlah siswa yang ikut?: "
+    read siswa
+
+    echo "Berapa biaya transportasi per siswa? (dalam rupiah): "  
+    read transcost
+
+    echo "Berapa biaya tiket masuk objek wisata per siswa? (dalam rupiah): "
+    read tickcost
+
+    echo "Berapa biaya konsumsi per siswa? (dalam rupiah): "
+    read conscost
+    
+    echo "Berapa anggaran yang dimiliki? (dalam rupiah): "
+    read modal
+
+    totaltrans=$((siswa * transcost))
+    totaltiket=$((siswa * tickcost))
+    totalkonsum=$((siswa * conscost))
+    totalbiaya=$((totaltrans + totaltiket + totalkonsum))
+    sisamodal=$((modal - totalbiaya))
+
+    echo "=========================="
+    echo "Jadi, total biaya untuk transportasi siswa adalah adalah: Rp $totalparkir" 
+    echo "Jadi, total biaya untuk tiket siswa adalah: Rp $totalparkir" 
+    echo "Jadi, total biaya untuk konsumsi siswa adalah: Rp $totalparkir" 
+    echo "=========================="
+    echo "Jadi, total biaya untuk perjalanan wisata sekolah adalah: Rp $totalbiaya"
+    echo "Jadi, sisa modalnya adalah: Rp $sisamodal" 
+
+    if [ $sisamodal -lt 0 ]; then
+        echo "Perjalanan ini melebihi anggaran sebesar Rp ${sisamodal#-}"
+    else
+        echo "Perjalanan ini dapat diselesaikan dengan anggaran yang ada."
+    fi
+}
+
+hitungharga
